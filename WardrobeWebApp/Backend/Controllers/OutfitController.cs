5203b386398caca758fefdd7b34bfe8933892afe
sz-99 <zhengsusan3@gmail.com>
2025-02-03 11:28:30 +0000

    Set up Controller for outfit

---
+++ WardrobeWebApp/Backend/Controllers/OutfitController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OutfitController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOutfit()
+        {
+            return Ok();
+        }
+    }
+}
