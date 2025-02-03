c26345bee53382dbe8d66ff508f81c6a50b178f5
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-02-03 14:47:22 +0000

    Create image controller

---
+++ WardrobeWebApp/Backend/Controllers/ImageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        public ImageController() { }
+
+        [HttpGet]
+
+    }
+}
