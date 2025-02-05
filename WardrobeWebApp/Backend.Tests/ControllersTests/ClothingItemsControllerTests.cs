57a81b3a82f0340b45c3945bcc923c95f6f27cca
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-02-05 15:25:24 +0000

    Start writing tests

---
+++ WardrobeWebApp/Backend.Tests/ControllersTests/ClothingItemsControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Tests.ControllersTests
+{
+    internal class ClothingItemsControllerTests
+    {
+        Mock<IClothing> _mockRepository;
+        ClothingItemsService _service;
+        ClothingItem _initialClothingItem;
+        ClothingItem _testClothingItem;
+    }
+}
