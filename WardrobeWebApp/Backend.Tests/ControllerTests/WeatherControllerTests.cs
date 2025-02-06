0b15eadd19ce28cbf2615bd6c7464957ec1c8403
sz-99 <zhengsusan3@gmail.com>
2025-02-06 13:40:22 +0000

    add tests for clothingItems and weather controller

---
+++ WardrobeWebApp/Backend.Tests/ControllerTests/WeatherControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Backend.Controllers;
+using Backend.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Tests.ControllerTests
+{
+    internal class WeatherControllerTests
+    {
+        private Mock<IWeatherService> _mockService;
+        private WeatherController _weatherController;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockService = new Mock<IWeatherService>();
+            _weatherController = new WeatherController(_mockService.Object);
+
+        }
+        [Test]
+        public void GetWeatherForecast_Returns_Ok()
+        {
+            //arrange
+
+
+            //act
+
+            //assert
+        }
+    }
+}
