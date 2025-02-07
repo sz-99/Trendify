949df21298b3c9a88edd66b44017980e339035b4
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-02-07 10:25:41 +0000

    In process of implementing EnumWrapper

---
+++ WardrobeWebApp/FrontEnd/FrontEnd/Components/Utils/EnumWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace FrontEnd.Components.Utils
+{
+    [TypeConverter(typeof(StringConverter))]
+    public class EnumWrapper<T> where T : Enum
+    {
+        public T Value { get; set; }
+        public EnumWrapper(T enumValue) 
+        { 
+            Value = enumValue;
+        }
+
+        public int AsInt { get => Convert.ToInt32(Value); }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+    }
+}
