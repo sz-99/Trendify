957e45ff7536685a432dbc0f0770a0b1eeaa1ed9
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-02-11 17:01:11 +0000

    Introduce JS functions

--- WardrobeWebApp/FrontEnd/FrontEnd/wwwroot/displayFunctions.js
+++ WardrobeWebApp/FrontEnd/FrontEnd/wwwroot/displayFunctions.js
@@ -1,3 +1,26 @@
-﻿function consoleLog() {
-  console.log("Hello")
+﻿var documentClicked = false;
+
+function hideAllDropdowns(e) {
+  if (e.target.id != "Category") {
+    try {
+      //document.querySelector("#Category-dropdown").setAttribute("hidden", "hidden")
+      documentClicked = true;
+    }
+    catch {
+
+    }
+  }
+  
+}
+
+document.addEventListener("click", (e) => {
+  hideAllDropdowns(e);
+})
+
+function getDocumentClicked() {
+  return documentClicked;
+}
+
+function setDocumentClicked(value) {
+  documentClicked = value;
 }
